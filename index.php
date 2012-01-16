0de685a6bb55f84be89c2fe67fba134cadcdbca4
keyjam <endou.ikeda@gmail.com>
2012-01-16 23:46:06 +0900

    commit

---
+++ index.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+<meta charset="Shift_JIS">
+<meta http-equiv="Content-Type" content="text/html; charset=Shift_JIS" />
+<meta name="author" content="html5">
+<meta name="copyright" content="Copyright &copy; 2001-2011.">
+<meta name="description" content="">
+<meta name="keywords" content="">
+<meta id="viewport" name="viewport" content="width=320; initial-scale=1.0; maximum-scale=1.0; user-scalable=0;" />
+<link rel="apple-touch-icon-precomposed" href=""/>
+<link rel="shortcut icon" href="" type="image/x-png" sizes="114x114">
+<link href="css/main.css" type="text/css" rel="stylesheet" />
+<script type="text/javascript" src="js/jquery-1.4.2.min.js"></script>
+<script type="text/javascript" src="js/imageLoop.js"></script>
+<title>html5</title>
+
+<script type="text/javascript">
+(function($) {
+	$.key.initBanner( [ '#banner1' ] );
+})(jQuery);
+
+</script>
+
+</head>
+<body>
+	<!--/header/-->
+	<header>
+		<div id="header" class="clear">
+			<h1>key-jam</h1>
+			<ul class="clear">
+				<li>create</li>
+			</ul>
+		</div>
+	</header>
+
+	<!--/navigation/-->
+	<nav>
+		<ul>
+			<li>jQuery</li>
+		</ul>
+	</nav>
+
+	<!--/content/-->
+	<section>
+
+		<div id="banner1" class="banner"></div>
+
+	</section>
+
+	<!--/footer/-->
+	<footer>
+		<h3>(C)2011 keyjam</h3>
+	</footer>
+</body>
+</html>
